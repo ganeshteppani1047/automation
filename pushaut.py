3c028f108b67c1e38bc5a599a81141e1fe4e554c
ganesh444 <ganeshteppani444@gmail.com>
2018-03-11 15:19:23 +0530

    febb

--- pushaut.py
+++ pushaut.py
@@ -7,7 +7,10 @@ def ganTe():
   global totalTime
   totalTime = round(endTime - startTime)
   while int(totalTime) > 5:
+    x= commands.getoutput("git add . https://ganeshteppani1047:ganesh123@github.com/ganeshteppani1047/rep.git")
+    x= commands.getoutput("git commit -m jf https://ganeshteppani1047:ganesh123@github.com/ganeshteppani1047/rep.git")
     x= commands.getoutput("git push https://ganeshteppani1047:ganesh123@github.com/ganeshteppani1047/rep.git")
+    
     print(x)
     ganTe()
     break
